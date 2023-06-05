529d5007c74a17bd884fdf76f4221d98f34362f9
MohammedAlameri <ma@MacBook-Pro-van-MA.local>
2023-06-05 21:44:42 +0200

    added editing Cohorts

--- src/main/resources/static/js/cohort.js
+++ src/main/resources/static/js/cohort.js
@@ -3,3 +3,16 @@ $(document).ready(function () {
     $(".active").removeClass("active");
     $("#cohorts").addClass("active");
 });
+
+/** update the values of the fields when we click on edit button **/
+function updateCohortModalForm(thisButton, cohortId, cohortName, cohortStatus, courseId) {
+
+    const modal = $('#cohort-modal');
+
+    // changing cohort fields
+    modal.find('#cohortId').val(cohortId)
+    modal.find('#cohortName').val(cohortName);
+    modal.find('#cohortStatus').val(cohortStatus);
+    modal.find('#cohortCourse').val(courseId);
+
+}
