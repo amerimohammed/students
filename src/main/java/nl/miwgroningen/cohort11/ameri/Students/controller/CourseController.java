3b5b79959e326bcc45d26a6be8364cb4986ce00e
MohammedAlameri <ma@MacBook-Pro-van-MA.local>
2023-06-05 22:50:27 +0200

    added handling deletion exception

--- src/main/java/nl/miwgroningen/cohort11/ameri/Students/controller/CourseController.java
+++ src/main/java/nl/miwgroningen/cohort11/ameri/Students/controller/CourseController.java
@@ -3,6 +3,8 @@ package nl.miwgroningen.cohort11.ameri.Students.controller;
 import lombok.RequiredArgsConstructor;
 import nl.miwgroningen.cohort11.ameri.Students.model.Course;
 import nl.miwgroningen.cohort11.ameri.Students.repository.CourseRepository;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Optional;
 
 /**
@@ -23,27 +26,34 @@ public class CourseController {
     private final CourseRepository courseRepository;
 
     @GetMapping({"/", "/course/overview"})
-    private String showCourseOverview(Model model){
+    private String showCourseOverview(Model model) {
         model.addAttribute("courses", courseRepository.findAll());
         model.addAttribute("course", new Course());
         return "courseOverview";
     }
 
     @PostMapping("/")
-    private String SaveUpdateCourse (@ModelAttribute("course") Course course, BindingResult result){
-           if(!result.hasErrors()){
-               courseRepository.save(course);
-           }
+    private String SaveUpdateCourse(@ModelAttribute("course") Course course, BindingResult result) {
+        if (!result.hasErrors()) {
+            courseRepository.save(course);
+        }
 
-           return "redirect:/";
+        return "redirect:/";
     }
 
     @GetMapping("/course/delete/{courseId}")
-    private String deleteCourse(@PathVariable("courseId") Long courseId) {
+    private String deleteCourse(@PathVariable("courseId") Long courseId, Model model) {
         Optional<Course> optionalCourse = courseRepository.findById(courseId);
 
         if (optionalCourse.isPresent()) {
-            courseRepository.delete(optionalCourse.get());
+            try {
+                courseRepository.delete(optionalCourse.get());
+            } catch (DataIntegrityViolationException dataIntegrityViolationException) {
+                System.out.println(dataIntegrityViolationException.getMessage());
+                model.addAttribute("errorMessage",
+                        "This Course can't be deleted due to relation with other entities");
+                return "error";
+            }
         }
 
         return "redirect:/";
